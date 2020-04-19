f162600a37e8fb025e4475f8df87efa40a95c69d
ha4219 <ha4219@naver.com>
2020-04-19 13:41:36 +0000

    #9.5

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand
+from django.contrib.admin.utils import flatten
 from rooms import models as room_models
 from users import models as user_models
 from django_seed import Seed
-import random 
+import random
 
 
 class Command(BaseCommand):
@@ -22,11 +23,20 @@ class Command(BaseCommand):
             'name': lambda x: seeder.faker.address(),
             'host': lambda x: random.choice(all_user),
             'room_type': lambda x: random.choice(room_types),
-            'guests': lambda x: random.randint(0, 20),
-            'price': lambda x: random.randint(0, 300),
-            'beds': lambda x: random.randint(0, 5),
-            'bedrooms': lambda x: random.randint(0, 5),
-            'baths': lambda x: random.randint(0, 5),
+            'guests': lambda x: random.randint(1, 20),
+            'price': lambda x: random.randint(1, 300),
+            'beds': lambda x: random.randint(1, 5),
+            'bedrooms': lambda x: random.randint(1, 5),
+            'baths': lambda x: random.randint(1, 5),
         })
-        seeder.execute()
+        created_photos = seeder.execute()
+        created_clean = flatten(list(created_photos.values()))
+        for pk in created_clean:
+            room = room_models.Room.objects.get(pk=pk)
+            for i in range(3, random.randint(10, 17)):
+                room_models.Photo.objects.create(
+                    caption=seeder.faker.sentence(),
+                    room=room,
+                    file=f"room_photos/{random.randint(1, 31)}.webp"
+                )
         self.stdout.write(self.style.SUCCESS(f"{number} Rooms created"))
