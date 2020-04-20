974ae36bb07f240f2624dcbc14b03c328d9af919
ha4219 <ha4219@naver.com>
2020-04-20 13:59:58 +0000

    #11.2

--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from . import models
+from math import ceil
 
 def all_rooms(request):
     page = int(request.GET.get('page', 1))
+    page = int(page or 1)
     page_size = 10
-    limit = page_size + page
+    limit = page_size * page
     offset = limit - page_size
     all_room = models.Room.objects.all()[offset: limit]
-    return render(request, 'rooms/home.html', context={'rooms': all_room})
+    page_count = ceil(models.Room.objects.count() / page_size)
+    return render(
+        request,
+        "rooms/home.html",
+        {
+            "rooms": all_room,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count),
+        },
+    )
