0afb458650f274fcf440e793f4cc1be9a117f005
ha4219 <ha4219@naver.com>
2020-04-21 13:54:06 +0000

    #11.9

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.utils import timezone
 from . import models
 
 
@@ -9,3 +10,10 @@ class HomeView(ListView):
     ordering = 'created'
     paginate_orphans = 5
     page_kwarg = 'page'
+    context_object_name = 'rooms'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context['now'] = now
+        return context
