6228d54563a9acfc880818d9659af45188317188
ha4219 <ha4219@naver.com>
2020-04-21 14:35:20 +0000

    #12.4

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,8 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.utils import timezone
 from django.urls import reverse
-from django.shortcuts import render, redirect
-from django.http import Http404
+# from django.shortcuts import render, redirect
+# from django.http import Http404
 from . import models
 
 
@@ -22,9 +22,14 @@ class HomeView(ListView):
         return context
     
     
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, 'rooms/detail.html', {'room':room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, 'rooms/detail.html', {'room':room})
+#     except models.Room.DoesNotExist:
+#         raise Http404()
+
+class RoomDetailView(DetailView):
+    ''' RoomDetailView Definition '''
+    model = models.Room
+    # pk_url_kwarg = 'photato'
