54b5bb750898217946011ccf79f63aaf3ab58f2e
ha4219 <ha4219@naver.com>
2020-04-23 13:10:50 +0000

    2020.04.23

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LoginView(View):
+    
+    def get(self, request):
+        pass
+    
+    def post(self, request):
+        pass
+    
+def login_view(request):
+    if request.method == "GET":
