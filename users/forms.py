ba4f83d4354be49b811a01046e4cce7bc085b018
ha4219 <ha4219@naver.com>
2020-04-23 14:42:17 +0000

    #14.6

--- users/forms.py
+++ users/forms.py
@@ -19,4 +19,18 @@ class LoginForm(forms.Form):
         except models.User.DoesNotExist:
             self.add_error('email', forms.ValidationError('User does not exist'))
             
-        
+
+class SignUpForm(forms.Form):
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError('User already exists with the email')
+        except models.User.DoesNotExist:
+            return email
--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('login', views.LoginView.as_view(), name='login'),
-    path('logout', views.log_out, name='logout')
+    path('logout', views.log_out, name='logout'),
+    path('sigup', views.SignUpView.as_view(), name='signup'),
 ]
