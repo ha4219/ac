89db85bcfd1e798d4904355085806dceedd88ee0
ha4219 <ha4219@naver.com>
2020-04-23 14:31:36 +0000

    #14.5

--- users/views.py
+++ users/views.py
@@ -1,25 +1,33 @@
 from django.views import View
+from django.views.generic import FormView
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse_lazy
 from . import forms
 
 
-class LoginView(View):
+class LoginView(FormView):
+    template_name = 'users/login.html'
+    form_class = forms.LoginForm
+    success_url = reverse_lazy('core:home')
     
-    def get(self, request):
-        form = forms.LoginForm()
-        return render(request, 'users/login.html', {'form':form})
-    
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse('core:home'))
-        return render(request, 'users/login.html', {'form': form})
+    def form_valid(self, form):
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+    # def post(self, request):
+    #     form = forms.LoginForm(request.POST)
+    #     if form.is_valid():
+    #         email = form.cleaned_data.get('email')
+    #         password = form.cleaned_data.get('password')
+    #         user = authenticate(request, username=email, password=password)
+    #         if user is not None:
+    #             login(request, user)
+    #             return redirect(reverse('core:home'))
+    #     return render(request, 'users/login.html', {'form': form})
     
 def log_out(request):
     logout(request)
