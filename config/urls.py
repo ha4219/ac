d806522576c723e6232b0f8de967405897c793fb
ha4219 <ha4219@naver.com>
2020-04-23 13:34:41 +0000

    #14.1 github server error

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
-    path('rooms/', include('rooms.urls', namespace='rooms'))
+    path('rooms/', include('rooms.urls', namespace='rooms')),
+    path('users/', include('users.urls', namespace='users'))
 ]
 
 if settings.DEBUG:
--- users/views.py
+++ users/views.py
@@ -1,13 +1,19 @@
 from django.views import View
+from django.shortcuts import render
+from . import forms
 
 
 class LoginView(View):
     
     def get(self, request):
-        pass
+        form = forms.LoginForm()
+        return render(request, 'users/login.html', {'form':form})
     
     def post(self, request):
-        pass
+        form = forms.LoginForm(request.POST)
+        
     
 def login_view(request):
     if request.method == "GET":
+        form = forms.LoginForm()
+        return render(request, 'users/login.html', {'form':form})
