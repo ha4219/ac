d0281794262ec7b93347297e2ac7ddb6ea49fbdc
ha4219 <ha4219@naver.com>
2020-04-24 13:48:48 +0000

    #15.2

--- users/models.py
+++ users/models.py
@@ -37,6 +37,6 @@ class User(AbstractUser):
     bio = models.TextField(default="", blank=True)
     #date
     birthdate = models.DateField(null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True)
-    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True, default=LANGUAGE_KOREAN)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True, default=CURRENCY_KRW)
     superhost = models.BooleanField(default=False)
