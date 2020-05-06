f8417b28a99e4fa8791e02e8745e0d136792a245
ha4219 <ha4219@naver.com>
2020-05-06 14:05:35 +0000

    #error

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from . import managers
 
 # Create your models here.
 
@@ -7,6 +8,6 @@ class TimeStampedModel(models.Model):
     """ Time Stamped Model """
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
+    objects = managers.CustomModelManager()
     class Meta:
         abstract = True
